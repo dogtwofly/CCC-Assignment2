cab0a28df00427e92db28a0308940f2f1c6060ae
Alan <alanwang24@gmail.com>
2019-05-03 16:22:47 +1000

    The final one

--- Twitter.py
+++ Twitter.py
@@ -1,42 +1,60 @@
-import requests
-import couchdb
+import tweepy
 import json
-from nltk.corpus import wordnet_ic
-
 import re
+import couchdb
+from nltk.corpus import wordnet_ic
 import nltk
 from profanity import profanity
 from nltk.corpus import wordnet as wn
 from nltk.stem import WordNetLemmatizer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import time
+from tweepy.streaming import StreamListener
+from tweepy import Stream
+
+#lin-similarity is caculated based on the brown.dat
 
 nltk.download('wordnet_ic')
 brown_info_content = wordnet_ic.ic('ic-brown.dat')
-# Delete Origin '_id' & '_rev'
-def add_id(tweetdata):
-    del saveitem['_rev']
-    del tweetdata['_id']
-    id = tweetdata['id_str']
-    tweetdata['_id'] = id
-    return tweetdata
+
+# My keys and tokens
+consumer_key = 'UjnHPModDzohpESefLeSCncAk'
+consumer_secret = '2OWQ9zYinzMZB3gUQx3o3tfIdt0JX2x00hRoBJqcxBN9I5E4nv'
+access_token = '988327751126925312-ts0LnQL22Cb1IOPnrHyobyu6BVWjTsO'
+access_token_secret = '9aRnHHOcLwVsHfw2264JebZQExWZKAZAxW2b6eOKGLF5D'
 
 
-# Sentiment Analysis
+# Get the authentication
+def getKeyToken():
+    access = {"consumer_key": consumer_key,
+            "consumer_secret": consumer_secret,
+            "access_token": access_token,
+            "access_secret": access_token_secret}
+
+    auth = tweepy.OAuthHandler(access['consumer_key'], access['consumer_secret'])
+    auth.set_access_token(access['access_token'], access['access_secret'])
+    return auth
+
+# analyze sentiment. input is string,return a dictionary including positive probability.
+
+#negative probability and when positive int, positive, negtive int ,negative. zero, neutual
+
 def sentiment_analysis(content):
-    senti_analyzer=SentimentIntensityAnalyzer()
-    return senti_analyzer.polarity_scores(content)
+
+    sentiAnalyzer = SentimentIntensityAnalyzer()
+    return sentiAnalyzer.polarity_scores(content)
 
 
+
+# check if the sentence have a profanity word.
 def profanity_analysis(content):
-    print("______________________________________")
-    print("This is test"+str(content))
-    print("______________________________________")
-    contain_profanity=profanity.contains_profanity(content)
+    containProfanity = profanity.contains_profanity(content)
+    return containProfanity
 
-    return contain_profanity
 
 
 # check if the word is relevant to crime
+
 def check_crime(word):
     max=0
     lemmatizer = WordNetLemmatizer()
@@ -57,7 +75,6 @@ def check_crime(word):
                 max=synset.lin_similarity(wn.synset(i), brown_info_content)
     return max
 
-
 def crime_analysis(content):
     tokens = nltk.word_tokenize(content.lower())
 
@@ -71,6 +88,7 @@ def crime_analysis(content):
         return True
     else:
         return False
+
 # check if the word is relevant to wrath
 def check_wrath(word):
     max = 0
@@ -91,8 +109,8 @@ def check_wrath(word):
                 max = synset.lin_similarity(wn.synset(i), brown_info_content)
     return max
 
-    word_a = lemmatizer.lemmatize(word, 'v')
-    for synset in wn.synsets(word_a, 'v'):
+    word_a = lemmatizer.lemmatize(word, 'a')
+    for synset in wn.synsets(word_a, 'a'):
         listadjective = ["angry.a.01", "mad.a.01", "unhappy.a.01"]
         for i in listadjective:
             if max < synset.lin_similarity(wn.synset(i), brown_info_content):
@@ -112,6 +130,7 @@ def wrath_analysis(content):
         return True
     else:
         return False
+
 # check if the word is relevant to lust
 def check_lust(word):
     max = 0
@@ -132,8 +151,8 @@ def check_lust(word):
                 max = synset.lin_similarity(wn.synset(i), brown_info_content)
     return max
 
-    word_a = lemmatizer.lemmatize(word, 'v')
-    for synset in wn.synsets(word_a, 'v'):
+    word_a = lemmatizer.lemmatize(word, 'a')
+    for synset in wn.synsets(word_a, 'a'):
         listadjective = ["sexual.a.01", "sexual.a.02", "sexy.a.01", "sexy.a.02", "jealous.a.01", "jealous.a.02"]
         for i in listadjective:
             if max < synset.lin_similarity(wn.synset(i), brown_info_content):
@@ -155,76 +174,46 @@ def lust_analysis(content):
         return False
 
 
-# Remove "http" information in tweet text
-def removehttp (tweet):
+def removehttp (tweetdata):
 
     try:
-        text = tweet['extended_tweet']['full_text']
+        text = tweetdata['extended_tweet']['full_text']
+
     except:
-        text = tweet['text']
+        text = tweetdata['text']
 
     pattern = re.compile('https://t.co/\w+')
+
     pat = pattern.findall(text)
+
     if len(pat) is 1:
         aa = pat[0]
         text = text.replace(aa, "")
-
     return text
 
 
-# Save to couchdb
-def savetodb(data):
-    address = "http://127.0.0.1:5984/" #just change the address to save the DB
-    couch = couchdb.Server(address)
-
-    try:
-        db = couch.create('newtweet') # create db table
-    except:
-        db = couch['newtweet']  #
-
-    try:
-        db.save(data)
-    except:
-        pass
+def add_id(tweetdata):
+    id=tweetdata['id']
+    tweetdata['_id']=str(id)
+    return tweetdata
 
 
+# Tweet listener
+class tweetListener(StreamListener):
 
+    def on_data(self, data):
 
-# Get Message
-url='http://45.113.232.90/couchdbro/twitter/_design/twitter/_view/summary'
-para={'include_docs':'true','reduce':'false','skip':'0','limit':'100'} #'start_key':'["perth", 2018, 1, 1]', 'end_key':'["perth", 2018, 12, 31]'}
+        tweetdata = json.loads(data.encode('utf-8'))
+        tweetdata = add_id(tweetdata)
 
-offset = 100
-current = 0
-
-while (current < 1000000):
-
-
-    message=requests.get(url,para,auth=('readonly', 'ween7ighai9gahR6'))
-
-
-    current = current + offset
-    temp = current
-    para['skip'] = str(temp)
-
-    dataset = message.json() # Message to dict
-    tweets = dataset['rows'] # List of tweets
-    print ("Done! "+ str(current) + "Tweets")
-
-
-    for tweet in tweets:
-        saveitem = tweet['doc']
-
-        id = saveitem['id_str']
-        content = saveitem['text']
-        # print(id)
-        saveitem = add_id(saveitem)
-        tweetReal = removehttp(saveitem)
-        # print (tweetReal)
+        tweetReal = removehttp(tweetdata)
         contentText1 = re.sub(r'#(\w+)\b', ' $1 ', tweetReal)
         contentText = re.sub(r'@\w+\b', '', contentText1)
 
-        # Call analysis functions
+        tweetid = tweetdata['id']
+        print(tweetid)
+
+
         sentiment = sentiment_analysis(contentText)
         try:
             profanity = profanity_analysis(contentText)
@@ -234,28 +223,67 @@ while (current < 1000000):
         wrath = wrath_analysis(contentText)
         lust = lust_analysis(contentText)
 
-
         # Update
-        saveitem['sentiment'] = sentiment
-        saveitem['profanity'] = profanity
-        if sentiment["compound"]<-0.5 and crime:
-            saveitem['crime'] = crime
+        tweetdata['sentiment'] = sentiment
+        tweetdata['profanity'] = profanity
+        if sentiment["compound"] < -0.5 and crime:
+            tweetdata['crime'] = crime
         elif not crime:
-            saveitem['crime'] = crime
+            tweetdata['crime'] = crime
+        else:
+            tweetdata['crime'] = not crime
 
-        if sentiment["neg"]>0 and wrath:
-            saveitem['wrath'] = wrath
+        if sentiment["neg"] > 0 and wrath:
+            tweetdata['wrath'] = wrath
         elif not wrath:
-            saveitem['wrath'] = wrath
+            tweetdata['wrath'] = wrath
+        else:
+            tweetdata['wrath'] = not wrath
 
-        if sentiment["neg"]>0.2 and lust:
-            saveitem['lust'] = lust
+        if sentiment["neg"] > 0.2 and lust:
+            tweetdata['lust'] = lust
         elif not lust:
-            saveitem['lust'] = lust
+            tweetdata['lust'] = lust
+        else:
+            tweetdata['lust'] = not lust
 
 
-        tweetsave = json.dumps(saveitem)
-        print(tweetsave)
 
-        # savetodb(saveitem)
+        tweetsave = json.dumps(tweetdata)
+        self.savetodb(tweetsave)
+        return True
 
+    def on_status(self, status):
+        print(status.text)
+
+    def on_error(self, status):
+        print(status)
+
+    def savetodb(self, data):
+        address = "http://127.0.0.1:5984/" #just change the address to save the DB
+        couch = couchdb.Server(address)
+        try:
+            db = couch.create('geotry') # create db table
+        except:
+            db = couch['geotry']
+
+        try:
+            db.save(json.loads(data.encode('utf-8')))
+        except:
+            pass
+
+
+def main():
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    try:
+        listener = tweetListener()
+        stream = Stream(getKeyToken(), listener)
+
+    # Only search tweets in restricted area and default language is English
+        stream.filter(languages=["en"], locations = [113.338953078, -43.6345972634, 153.569469029, -10.6681857235])
+    except Exception as e:
+        print(e)
+        time.sleep(60)
+
+main()
