4207f72b70e0ad2d9ed87f709e6c4ef3848dd4f3
saaltfiish <dogtwofly@gmail.com>
2019-05-11 18:41:09 +1000

    Create web.sh

---
+++ service/web.sh
@@ -0,0 +1,5 @@
+git clone -v https://github.com/dogtwofly/xanthic.git
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt install nodejs
+npm install
+nohup node ~/xanthic/service/app.js
