f5cb5b78d10f5954a7f5e31bd45088793a9bd057
saaltfiish <dogtwofly@gmail.com>
2019-05-11 23:13:29 +1000

    remove former web server

--- service/web.sh
+++
@@ -1,5 +0,0 @@
-git clone -v https://github.com/dogtwofly/xanthic.git
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-sudo apt install nodejs
-npm install
-nohup node ~/xanthic/service/app.js
