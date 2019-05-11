c013914330319c3f0083b7df2b2adf09e000ebe1
saaltfiish <dogtwofly@gmail.com>
2019-05-11 19:12:59 +1000

    Update web.sh

--- service/web.sh
+++ service/web.sh
@@ -1,5 +1,7 @@
 git clone -v https://github.com/dogtwofly/xanthic.git
+cd ~/xanthic/service
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 sudo apt install nodejs
 npm install
-nohup node ~/xanthic/service/app.js
+sudo npm install -g forever
+forever start app.js
