c1f0aa9f2241d85f3191b1c458d2c38850272c42
Vapidityyyyy <wthfrom1995@gmail.com>
2019-05-11 16:08:48 +1000

    final

--- ansible/deployment/docker-couchdb.sh
+++ ansible/deployment/docker-couchdb.sh
@@ -5,7 +5,7 @@ docker tag couchdb couchdb:local
 docker images -a
 docker run --name couchdb -e COUCHDB_USER=saaltfiish -e COUCHDB_PASSWORD=huhuahua0124 -p 5984:5984 -d couchdb:local
 docker ps -a
-sleep 10
+sleep 150
 curl -X PUT http://saaltfiish:huhuahua0124@127.0.0.1:5984/_users
 curl -X PUT http://saaltfiish:huhuahua0124@127.0.0.1:5984/_replicator
 curl -X PUT http://saaltfiish:huhuahua0124@127.0.0.1:5984/_global_changes
